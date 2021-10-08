5e74fd05e7d3d347c94c5d737de0a9cb9765145c
AntonLad <toxa.lada@gmail.com>
2021-10-08 16:59:44 +0300

    first try3

--- client/components/dashboard-profile.js
+++ client/components/dashboard-profile.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Head from './head'
+import Header from './header'
 
 const DashboardProfile = () => {
   const { user } = useParams()
   return (
     <div>
       <Head title="Hello" />
+      <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title"> Profile </div>
