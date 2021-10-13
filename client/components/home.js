8f110b4267685d8c507dde056efc64fd162813a1
AntonLad <toxa.lada@gmail.com>
2021-10-13 21:31:03 +0300

    upload finall

--- client/components/home.js
+++ client/components/home.js
@@ -1,49 +1,22 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
-import { Provider, connect } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router'
-import { /* Link, */ Switch, Route, withRouter /* , Redirect */ } from 'react-router-dom'
-import { bindActionCreators } from 'redux'
 
-import store, { history } from '../redux'
+import { Switch, Route } from 'react-router-dom'
 
-// import { Link } from 'react-router-dom'
 import Header from './header'
 import Dashboard from './dashboard'
-// import Home from './home'
+
 import DashboardMain from './dashboard-main'
 import DashboardProfile from './dashboard-profile'
-import Startup from '../config/startup'
-
-// const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch)
-
-// const PrivateRouteConnected = connect(mapStateToProps, mapDispatchToProps)(PrivateRoute)
-
-const mapDispatchToPropsStartup = (dispatch) => bindActionCreators({}, dispatch)
-
-const StartupConnected = withRouter(connect(() => ({}), mapDispatchToPropsStartup)(Startup))
-
-const RootComponent = (props) => {
-  return (
-    <Provider store={store}>
-      <ConnectedRouter history={history} location={props.location} context={props.context}>
-        <StartupConnected>
-          <Switch>
-            <Route exact path="/dashboard/" component={() => <Dashboard />} />
-            <Route exact path="/dashboard/profile/:user" component={() => <DashboardProfile />} />
-            <Route exact path="/dashboard/main" component={() => <DashboardMain />} />
-          </Switch>
-        </StartupConnected>
-      </ConnectedRouter>
-    </Provider>
-  )
-}
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <RootComponent />
+      <Switch>
+        <Route exact path="/dashboard/" component={() => <Dashboard />} />
+        <Route exact path="/dashboard/profile/:user" component={() => <DashboardProfile />} />
+        <Route exact path="/dashboard/main" component={() => <DashboardMain />} />
+      </Switch>
     </div>
   )
 }
